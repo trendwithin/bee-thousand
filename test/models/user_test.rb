1cf789bd8b809d142f903f0a11f23ba84586b931
Michael Becco <e4e6d4d5@gmail.com>
2017-04-20 15:28:19 -0700

    Add User Model Validation Check

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(email: 'user_test@example.com', password: 'password')
+  end
+
+  test 'Valid User' do
+     assert @user.valid?
+     assert_equal 'pending', @user.registration_status
+     assert_equal 'standard', @user.role
+  end
+
+  test 'User with Edge Case Password Length' do
+    @user.password = '1234567'
+    refute @user.valid?
+    @user.password = 'a' * 129
+    refute @user.valid?
+  end
 end
