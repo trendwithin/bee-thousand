2fc19f3ca2df1d959b6aad5f8644afc18f504bfe
Michael Becco <e4e6d4d5@gmail.com>
2017-04-20 15:06:59 -0700

    Test User Sign Up Registration Status

--- test/integration/sign_up_test.rb
+++ test/integration/sign_up_test.rb
@@ -3,12 +3,14 @@ require 'test_helper'
 class SignUpTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
-  test 'Successful Sgnup of User' do
+  test 'Successful Sign Up of User' do
     get new_user_registration_path
     assert_response :success
     assert_equal '/users/sign_up', path
     assert_difference 'User.count', 1 do
       post user_registration_path, params: { user: { email: 'signuptest@example.com', password: 'password' } }
     end
+    user = User.find_by(email: 'signuptest@example.com')
+    assert_equal 'pending', user.registration_status
   end
 end
