b0eee7a4c7893cac34fea445a94c7b37fa74f49c
Michael Becco <e4e6d4d5@gmail.com>
2017-04-20 15:57:28 -0700

    Add #deliver UserMailer/AdminMailer

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,8 @@ class RegistrationsController < Devise::RegistrationsController
   def create
     super
     if @user.persisted?
-      UserMailer.account_registration(@user)
+      UserMailer.account_registration(@user).deliver
+      AdminMailer.user_register_notification.deliver
     end
   end
 end
