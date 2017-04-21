b43b12077051d4bac7c1bdf6f8690ed23b7ca210
Michael Becco <e4e6d4d5@gmail.com>
2017-04-21 16:15:18 -0700

    Add About Page Template

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,12 @@
 class PagesController < ApplicationController
+  layout "page"
   before_action :authenticate_user!, except: :welcome
   def welcome
   end
 
   def registered
+  end
+
+  def about
   end
 end
