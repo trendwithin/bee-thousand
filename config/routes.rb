f37284dea43819dda08c59171d455ae365058635
Michael Becco <e4e6d4d5@gmail.com>
2017-04-24 12:18:35 -0700

    Test Welcome Page Links, Add Correct Links and Corresponding Routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'pages#welcome'
   get 'pages/registered'
   get 'pages/about'
+  get 'pages/contact'
+  get 'pages/testimonial'
 
   devise_for :users, controllers: { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -6,9 +6,24 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test 'Should have Sign Up Button' do
+  test 'Should have Sign Up/Learn More Button' do
     get root_path
     assert_response :success
     assert_select 'a[href=?]', new_user_registration_path, 'Sign Up'
+    assert_select 'a[href=?]', pages_about_path, 'Learn More'
+  end
+
+  test 'Should Have Contact About Testimonials Login Links' do
+    get root_path
+    assert_select 'a[href=?]', pages_contact_path, 'Contact'
+    assert_select 'a[href=?]', pages_about_path, 'About'
+    assert_select 'a[href=?]', pages_testimonial_path, 'Testimonials'
+    assert_select 'a[href=?]', new_user_session_path, 'Login'
+  end
+
+  test 'Should Not Have Logout Unless Signed In' do
+    get root_path
+    assert_response :success
+    assert_select 'a[href=?]', destroy_user_session_path, count: 0
   end
 end
