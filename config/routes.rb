6a349f1981d64b1e29770c7d3b6c9efb3fc9e2c4
Michael Becco <e4e6d4d5@gmail.com>
2017-05-01 11:41:49 -0700

    Add Routes for Micropost #create #destroy

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'pages/about'
   get 'pages/contact'
   get 'pages/testimonial'
+  resources :microposts, only: [:create, :destroy]
 
   devise_for :users, controllers: { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
