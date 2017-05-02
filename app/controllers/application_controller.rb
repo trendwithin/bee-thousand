9dfce6ade043c979a2053a4609b29875d6e17819
Michael Becco <e4e6d4d5@gmail.com>
2017-05-02 14:02:00 -0700

    Modify Controllers For Pundit Auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
+  include Pundit
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   protect_from_forgery with: :exception
+
+  private
+  def user_not_authorized
+    self.response_body = nil
+    flash[:alert] = 'A problem has occured.  Please email Admin if you feel this in error.'
+    redirect_to(request.referrer || root_path)
+  end
 end
--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,7 +1,10 @@
 class MicropostsController < ApplicationController
+  before_action :authenticate_user!
+  after_action :verify_authorized
 
   def create
-    @micropost = User.first.microposts.build(micropost_params)
+    @micropost = current_user.microposts.build(micropost_params)
+    authorize @micropost
     if @micropost.save
       flash[:notice] = 'Posted Successfully.'
       redirect_to timelines_index_path
@@ -13,6 +16,7 @@ class MicropostsController < ApplicationController
 
   def destroy
     @micropost = Micropost.find(params[:id])
+    authorize @micropost
     @micropost.destroy
     redirect_to timelines_index_path, notice: 'Post Deleted.'
   end
