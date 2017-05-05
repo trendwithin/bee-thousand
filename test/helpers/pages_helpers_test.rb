e15b44894f5574f3b60ec0d59d8c206589e68f41
Michael Becco <e4e6d4d5@gmail.com>
2017-05-05 15:36:55 -0700

    Add PagesHelper Test

--- test/helpers/pages_helpers_test.rb
+++ test/helpers/pages_helpers_test.rb
@@ -24,4 +24,17 @@ class PagesHelperTest < ActionView::TestCase
   test 'nav_items returns correct values' do
    assert_equal items, nav_items
   end
+
+  test 'active? page returns true for current_page?' do
+    fake_view = Object.new
+    class << fake_view
+      include PagesHelper
+
+      def current_page?(path)
+        path == 'pages/about'
+      end
+    end
+    assert_equal 'active', fake_view.active?('pages/about')
+    assert_nil fake_view.active?('pages/other')
+  end # Thanks for the test suggestion Chris Kottom!
 end
