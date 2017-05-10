129aa20837a361b5e4cebbbecd603949ce5e52cf
Michael Becco <e4e6d4d5@gmail.com>
2017-05-10 09:19:58 -0700

    Pass #create for AlertsController

---
+++ app/controllers/alerts_controller.rb
@@ -0,0 +1,16 @@
+class AlertsController < ApplicationController
+  def create
+    @alert = current_user.alerts.build(alert_params)
+    if @alert.save
+      flash[:notice] = 'Alert Posted Successfully.'
+      redirect_to timelines_index_path
+    else
+      render 'timelines/index', layout: 'timeline'
+    end
+  end
+
+  private
+    def alert_params
+      params.require(:alert).permit(:symbol, :entry, :stop, :target, :shares, :comment)
+    end
+end
